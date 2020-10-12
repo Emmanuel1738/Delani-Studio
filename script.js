7a91d762d23a2b4b5c2711d0676b666aaa2bd019
Emmanuel1738 <emmanuelbett13@gmail.com>
2020-10-12 08:40:39 +0300

    declared variables

---
+++ script.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var key = "http://eepurl.com/gIAK4r";
+    if (name == "") {
+      alert("please fill in your name");
+      return false;
+    } else if (email == "") {
+      alert("Please fill in your name and email");
+      return false;
+    } else {
+      alert(
+        name +
+          ", we have received your message. Thank you for reaching out to us."
+      );
+    }
+    
